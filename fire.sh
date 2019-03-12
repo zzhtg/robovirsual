a182e9ecba4d2acd40df58c7dc5b348d32ebc2de
zzh <2559437829@qq.com>
2019-03-12 22:46:14 +0800

    rename fire.sh

--- fire.sh
+++
@@ -1,7 +0,0 @@
-mkdir ~/manifold
-cd ~/Downloads
-wget --no-check-certificate https://dl.djicdn.com/downloads/manifold/manifold_image_v1.0.tar.gz
-cd ~/manifold
-tar -xvpzf ~/Downloads/manifold_image_v1.0.tar.gz
-cd ~/manifold/Linux_for_Tegra
-./flash jetson-tk1 mmcblk0p1
