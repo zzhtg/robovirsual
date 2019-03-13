b7219aaa26178c06da8ce753adbb0e0141f21ac0
zzh <2559437829@qq.com>
2019-03-13 20:13:18 +0800

    update readme

--- install.sh
+++ install.sh
@@ -55,37 +55,3 @@ make -j4
 make install
 
 pip3 insatll pyserial
-
-#cmake \
-#    -DCMAKE_BUILD_TYPE=Release \
-#    -DCMAKE_INSTALL_PREFIX=/usr \
-#    -DCMAKE_CXX_FLAGS=-Wa,-mimplicit-it=thumb \
-#    -DBUILD_PNG=OFF \
-#    -DBUILD_TIFF=OFF \
-#    -DBUILD_TBB=OFF \
-#    -DBUILD_JPEG=OFF \
-#    -DBUILD_JASPER=OFF \
-#    -DBUILD_ZLIB=OFF \
-#    -DBUILD_EXAMPLES=ON \
-#    -DBUILD_opencv_java=OFF \
-#    -DBUILD_opencv_python2=ON \
-#    -DBUILD_opencv_python3=ON \
-#    -DENABLE_NEON=ON \
-#    -DWITH_OPENCL=OFF \
-#    -DWITH_OPENMP=OFF \
-#    -DWITH_FFMPEG=ON \
-#    -DWITH_GSTREAMER=OFF \
-#    -DWITH_GSTREAMER_0_10=OFF \
-#    -DWITH_CUDA=ON \
-#    -DWITH_GTK=ON \
-#    -DWITH_VTK=OFF \
-#    -DWITH_TBB=ON \
-#    -DWITH_1394=OFF \
-#    -DWITH_OPENEXR=OFF \
-#    -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-7.0 \
-#    -DCUDA_ARCH_BIN=3.2 \
-#    -DCUDA_ARCH_PTX="" \
-#    -DINSTALL_C_EXAMPLES=ON \
-#    -DINSTALL_TESTS=OFF \
-#    -DOPENCV_TEST_DATA_PATH=../opencv_extra/testdata \
-#    ../opencv
