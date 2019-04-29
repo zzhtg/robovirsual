894313895e5a03131adce507441d2f236019602a
palezero <593153252@qq.com>
2019-04-29 20:38:51 +0800

    modify the svm_data absolute file and quick run in desktop, modify the serial quit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ frame_y = 1080
 midx = 320      # 非全视窗模式分辨率
 midy = 240
 ad.debug_mode = True
-serial_give = False
+serial_give = True
 ser = 0
 key = 0
 
@@ -58,13 +58,13 @@ def stm32(debug_mode=False):
                 print("You have sent", ord(serial_msg[0]), serial_msg[1], serial_msg[2], serial_msg[3], ord(serial_msg[4]), serial_msg[5], serial_msg[6], serial_msg[7], ord(serial_msg[-1]),"to the serial, x = ", midx, "y = ", midy)
             time.sleep(0.005)
             if key is ord('q'):
-                break 
+                return 
         except:
             try:
                 ser = ss.serial_init(115200, 1)
                 print("Here is no serial device, waiting for a connection...") 
                 if key is ord('q') :
-                    break
+                    return
             except:
                 pass
 
@@ -76,7 +76,7 @@ def main():
     global key, serial_give, cap
     color = 98  # 114: red, 98: blue
     tiktok = pf.Tiktok()                    # 滴答计时器
-    svm = cv2.ml.SVM_load('./svm_data.dat') # 读取svm参数
+    svm = cv2.ml.SVM_load('/home/hushi/robovirsual/svm_data.dat') # 读取svm参数
     kalman = kp.Kalman_Filter()             # 卡尔曼滤波器类初始化
     frame = pf.Frame(640, 480, frame_x, frame_y,
     EntireWindow, tiktok, focus = True, success=True,
@@ -102,6 +102,7 @@ def main():
 
         _k, color = pf.key_detect(cap, color)
         if(_k):
+            key = 113
             break
 
         tiktok.tok()
